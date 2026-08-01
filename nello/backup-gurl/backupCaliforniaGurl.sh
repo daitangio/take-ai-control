97092ac2ec382f4046ef27de0d742085ebd0a6d2
Giovanni Giorgi (macmini) <jj@gioorgi.com>
2026-08-01 09:33:45 +0200

    Monthly backup

--- nello/backup-gurl/backupCaliforniaGurl.sh
+++ nello/backup-gurl/backupCaliforniaGurl.sh
@@ -3,14 +3,22 @@
 # Backup nello database mounted under /app/nello/nello.db
 # Do a backup every 5 minutes
 # Rotate backup every week-day, to have up to 7 backups
-INTERVAL_MINUTES=15
+INTERVAL_MINUTES=45
 
 
 while true; do
     BACKUP_FILE=nello-backup-$(date +%w).sql
     echo -n $BACKUP_FILE .
     sqlite3  /app/nello/nello.db .dump >/app/nello/$BACKUP_FILE
+    echo -n .
+    
+    # Apply also the monthly backup, copying this one (less impact)
+    BACKUP_FILE_MONTHLY=nello-monthly-backup-$(date +%m).sql
+    cp /app/nello/$BACKUP_FILE /app/nello/$BACKUP_FILE_MONTHLY
+
     echo ..DONE
     sleep $(( $INTERVAL_MINUTES*60))
+    
+
 done    
 
